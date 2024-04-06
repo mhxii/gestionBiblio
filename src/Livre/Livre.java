708adf1cf8dab0be8faafbfcbf9cd0cea0cdc17d
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-06 12:38:07 +0000

    getter et setter pour le titre

--- src/Livre/Livre.java
+++ src/Livre/Livre.java
@@ -9,11 +9,22 @@ public class Livre {
     private String ISBN;
 
     // Constructeur pour initialiser les attributs
-    public Livre(String titre, String auteur, int anneePublication, String ISBN) {
+    public Livre(String titre, String auteur, int anneePublication, String ISBN){
         this.titre = titre;
         this.auteur = auteur;
         this.anneePublication = anneePublication;
         this.ISBN = ISBN;
+    }
+    //---------------------------------------
+    //Parties des getters er setters        |
+    //---------------------------------------
+
+    //Pour le titre
+    public String getTitre(){
+        return titre;
+    }
+    public void setTitre(String titre){
+        this.titre=titre;
     }
     
 }
