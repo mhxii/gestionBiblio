7da0803204b04de323930376cebcd9e202fa9c6e
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-06 12:48:09 +0000

    getter et setter pour ISBN

--- src/Livre/Livre.java
+++ src/Livre/Livre.java
@@ -43,4 +43,13 @@ public class Livre {
         this.anneePublication=anneePublication;
     }
 
+    //Pour ISBN
+    public String getISBN(){
+        return ISBN;
+    }
+    public void setISBN(String ISBN){
+        this.ISBN=ISBN;
+    }
+    
+
 }
