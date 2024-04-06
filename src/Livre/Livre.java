a797eaa790decc1a3a922c1f4ad55ab59cec6d96
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-06 12:14:02 +0000

    declaration des attributs de livre

--- src/Livre/Livre.java
+++ src/Livre/Livre.java
@@ -1,5 +1,11 @@
 package Livre;
 
 public class Livre {
+
+    //declarons les attributs d'abord
+    private String titre;
+    private String auteur;
+    private int anneePublication;
+    private String ISBN;
     
 }
