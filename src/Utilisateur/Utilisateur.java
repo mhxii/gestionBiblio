d6fbaffffbb06acac71a095674b1b4aa9b110564
medz1511 <134383184+medz1511@users.noreply.github.com>
2024-04-07 20:53:48 +0000

    declaration des attriburts de la  classe Utilisateur

--- src/Utilisateur/Utilisateur.java
+++ src/Utilisateur/Utilisateur.java
@@ -1,5 +1,23 @@
 package Utilisateur;
 
+import java.util.ArrayList;
+
+import Livre.Livre;
+
 public class Utilisateur {
-    
+
+    //Déclaration des attributs de la classe utilisateur
+    String nom ;
+    int numeroIdentification;
+    ArrayList<Livre> livresempruntes;
+
+    //
+
+
+
+
+
+   
+
+
 }
